eb596836e892a1d71f04a65544cf12fad2b31553
wellington Bastos <wellingtonbastos@hotmail.com>
2020-04-24 02:14:11 +0000

    add develop

--- handler.py
+++ handler.py
@@ -14,7 +14,7 @@ def main(event, context):
     
     
     responsePipeline = codepipeline.start_pipeline_execution(
-        name='hackathon-CD-app-stack-prd',
+        name='hackathon-CD-app-stack-stg,
     )
     
     response = {
